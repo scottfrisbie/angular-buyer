328b6912d67f24149b865ef0caea2be41a82912a
Alexa Rarick <ararick@four51.com>
2017-03-22 14:41:51 -0500

    Update base.js
    Set order.xp.ExpeditedShipping to "ground"

--- src/app/base/base.js
+++ src/app/base/base.js
@@ -106,7 +106,7 @@ function NewOrderService($q, OrderCloud) {
 
         function createOrder() {
             order.xp = {
-                ExpeditedShipping: "",
+                ExpeditedShipping: "ground",
                 sellerOrderID: 0
             };
             OrderCloud.Orders.Create(order)
