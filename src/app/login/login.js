f620bd414d8857e60a535fbf68af1f5f1f441cc9
Crhistian Ramirez <cramirez@four51.com>
2017-05-04 21:14:14 -0500

    revert reset password flow for now
    can think of how to implement this better later.
    firstLogin don’t have access to Me.Get()

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -107,18 +107,13 @@ function LoginController($state, $stateParams, $exceptionHandler, OrderCloudSDK,
             .then(function(data) {
                 OrderCloudSDK.SetToken(data.access_token);
                 if (vm.rememberStatus) OrderCloudSDK.SetRefreshToken(data['refresh_token']);
-                return OrderCloudSDK.Me.Get()
-                    .then(function(user){
-                        var roles = ocRolesService.Set(data.access_token);
-                        if (roles.length === 1 && roles[0] === 'PasswordReset') {
-                            vm.token = data.access_token;
-                            vm.form = 'resetByToken';
-                        } else if( (user && !user.xp) || (user && user.xp && !user.xp.HasChangedPW) ){
-                            vm.form = 'firstLogin';
-                        } else {
-                            $state.go('home');
-                        }
-                    });
+                var roles = ocRolesService.Set(data.access_token);
+                if (roles.length === 1 && roles[0] === 'PasswordReset') {
+                    vm.token = data.access_token;
+                    vm.form = 'resetByToken';
+                } else {
+                    $state.go('home');
+                }
             })
             .catch(function(ex) {
                 $exceptionHandler(ex);
